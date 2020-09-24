1162a1c9f15b508a44f7722e3a59f954ff2fe504
akashannex <akash@annexlogics.com>
2020-09-24 13:14:33 +0530

    Updates

---
+++ precache-manifest.5bd8f793c08fb980280acaf6c2cc22a1.js
@@ -0,0 +1,117 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e276333b841820b3c9b3fadab119bb5d",
+    "url": "/businesslivewebsite/index.html"
+  },
+  {
+    "revision": "7ff1a1ba54d98274c479",
+    "url": "/businesslivewebsite/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "e4928eb3ae035d045f25",
+    "url": "/businesslivewebsite/static/css/main.21d88d49.chunk.css"
+  },
+  {
+    "revision": "7ff1a1ba54d98274c479",
+    "url": "/businesslivewebsite/static/js/2.b2c9b741.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/businesslivewebsite/static/js/2.b2c9b741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4928eb3ae035d045f25",
+    "url": "/businesslivewebsite/static/js/main.f3d3daee.chunk.js"
+  },
+  {
+    "revision": "87b3b6fbdf7ba493b10b",
+    "url": "/businesslivewebsite/static/js/runtime-main.286fed42.js"
+  },
+  {
+    "url": "/businesslivewebsite/static/media/about-img-522fb8575d8fffe0f2bada7e70867aeb.522fb857.png"
+  },
+  {
+    "url": "/businesslivewebsite/static/media/banner-bg-b9c2a08e00981c080b506299a7d330e0.b9c2a08e.png"
+  },
+  {
+    "url": "/businesslivewebsite/static/media/bannerimg-7619177a0d2dd908b2c81557dd400626.7619177a.png"
+  },
+  {
+    "url": "/businesslivewebsite/static/media/bg-613f75d0989c6beafda899d1b437e961.613f75d0.png"
+  },
+  {
+    "url": "/businesslivewebsite/static/media/bg-img-37842434bf8e5cf09e0ac1107be6b148.37842434.png"
+  },
+  {
+    "revision": "6a911a3e622f23894af85e463d6eef07",
+    "url": "/businesslivewebsite/static/media/blog1.6a911a3e.png"
+  },
+  {
+    "revision": "fa1c50f136114f174f7cd1c8235924e2",
+    "url": "/businesslivewebsite/static/media/blog2.fa1c50f1.png"
+  },
+  {
+    "revision": "09197a28608e688e493c0505eb787fcc",
+    "url": "/businesslivewebsite/static/media/blog3.09197a28.png"
+  },
+  {
+    "revision": "baff6a6b626f175d889028b90c66f72b",
+    "url": "/businesslivewebsite/static/media/footer-bg.baff6a6b.png"
+  },
+  {
+    "revision": "f299f868d679d90a5e91f80fa07672d2",
+    "url": "/businesslivewebsite/static/media/form.f299f868.png"
+  },
+  {
+    "revision": "c59c5b5e551276a921e811a3af3230f0",
+    "url": "/businesslivewebsite/static/media/portfolio1.c59c5b5e.png"
+  },
+  {
+    "revision": "0b8ca8343410d5f22dd9e2570ed5c77d",
+    "url": "/businesslivewebsite/static/media/portfolio2.0b8ca834.png"
+  },
+  {
+    "revision": "7b84c0d9a923f6223de9c1282f39401c",
+    "url": "/businesslivewebsite/static/media/portfolio3.7b84c0d9.png"
+  },
+  {
+    "revision": "c68cd653ebb8c548b6a2d6c843d96232",
+    "url": "/businesslivewebsite/static/media/portfolio4.c68cd653.png"
+  },
+  {
+    "revision": "915617f2ea6f8bfe1c36a2946ec54d01",
+    "url": "/businesslivewebsite/static/media/portfolio5.915617f2.png"
+  },
+  {
+    "revision": "482a133d12a26cd6a4c4612949b92dc0",
+    "url": "/businesslivewebsite/static/media/portfolio6.482a133d.png"
+  },
+  {
+    "revision": "69e996e425465e3e545d85c9df277792",
+    "url": "/businesslivewebsite/static/media/skills-img.69e996e4.png"
+  },
+  {
+    "revision": "bd43e1d4f33a137ae28778521bab00e1",
+    "url": "/businesslivewebsite/static/media/testimonial1.bd43e1d4.png"
+  },
+  {
+    "revision": "7a99d0e488a848bf554efd92b3c039a3",
+    "url": "/businesslivewebsite/static/media/testimonial2.7a99d0e4.png"
+  },
+  {
+    "revision": "c0f26c4f6e2bc352eb4cf1c2b308e74a",
+    "url": "/businesslivewebsite/static/media/testimonial3.c0f26c4f.png"
+  },
+  {
+    "revision": "f1a9ca59fc7b91e606485f847a71279a",
+    "url": "/businesslivewebsite/static/media/testimonial4.f1a9ca59.png"
+  },
+  {
+    "revision": "77539c93985b56a3ba4c4bd335207b44",
+    "url": "/businesslivewebsite/static/media/testimonial5.77539c93.png"
+  },
+  {
+    "revision": "8d3cfbeeb668ccd709ff0dc4b3394727",
+    "url": "/businesslivewebsite/static/media/testimonial6.8d3cfbee.png"
+  }
+]);
